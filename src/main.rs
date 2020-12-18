cffd4e2a7b8dcbadaf7e2bc74829cf4e06a3e43a
hhggit <zh.xz@outlook.com>
2020-12-18 11:16:46 +0800

    tui

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,82 @@
 use std::{
     collections::HashMap,
+    fmt::{Debug, Formatter},
     io::{stdin, BufRead, BufReader},
+    rc::Rc,
 };
 
 use anyhow::Result;
+use cursive::{traits::*, views::Dialog, Cursive};
+use cursive_tree_view::{Placement, TreeView};
 use indextree::{Arena, NodeId};
 use once_cell::sync::Lazy;
 use regex::Regex;
 
 fn main() -> Result<()> {
     let (arena, root) = parse_tree(BufReader::new(stdin()))?;
+
+    #[derive(Debug)]
+    struct TreeEntry {
+        node: NodeId,
+        arena: Rc<Arena<String>>,
+    }
+
+    impl TreeEntry {
+        fn as_str(&self) -> &str {
+            self.arena.get(self.node).unwrap().get()
+        }
+    }
+
+    impl std::fmt::Display for TreeEntry {
+        fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+            f.write_str(self.as_str())
+        }
+    }
+
+    fn expand_tree(tree: &mut TreeView<TreeEntry>, row: usize) {
+        let e = tree.borrow_item(row).unwrap();
+        let v: Vec<_> = e
+            .node
+            .children(&e.arena)
+            .map(|i| TreeEntry {
+                node: i,
+                arena: e.arena.clone(),
+            })
+            .collect();
+        for i in v {
+            if i.node.children(&i.arena).next().is_some() {
+                tree.insert_container_item(i, Placement::LastChild, row);
+            } else {
+                tree.insert_item(i, Placement::LastChild, row);
+            }
+        }
+    }
+
+    let mut tree = TreeView::<TreeEntry>::new();
+
+    tree.insert_item(
+        TreeEntry {
+            node: root,
+            arena: Rc::new(arena),
+        },
+        Placement::After,
+        0,
+    );
+
+    expand_tree(&mut tree, 0);
+
+    tree.set_on_collapse(|siv: &mut Cursive, row, is_collapsed: bool, children| {
+        if !is_collapsed && children == 0 {
+            siv.call_on_name("tree", move |tree: &mut TreeView<TreeEntry>| {
+                expand_tree(tree, row);
+            });
+        }
+    });
+
+    let mut siv = Cursive::default();
+    siv.add_layer(Dialog::around(tree.with_name("tree")));
+
+    siv.run();
 
     Ok(())
 }
